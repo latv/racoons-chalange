1e2d016376d2fda8f0b5ec04a350af7f6f5b5c32
Janis Feldmanis <feldmanis45@gmail.com>
2020-04-12 20:10:14 +0300

    added grid

---
+++ main.js
@@ -0,0 +1,30 @@
+function draw() {
+
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var context = canvas.getContext('2d');
+
+    for(var x=0.5;x<500;x+=10) {
+      context.moveTo(x,0);
+      context.lineTo(x,500);
+    }
+
+    for(var y=0.5; y<500; y+=10) {
+      context.moveTo(0,y);
+      context.lineTo(500,y);
+
+  }
+
+  context.strokeStyle='grey';
+  context.stroke();
+
+}
+}
+
+function showCoords(event) {
+var x = event.clientX - 10;
+var y = event.clientY - 10;
+var coords = "X coordinates: " + x + ", Y coordinates: " + y;
+document.getElementById('showCoords').innerHTML = coords;
+
+}
